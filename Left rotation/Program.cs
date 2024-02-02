06e5efa685c843d2f1b5a058be50cc5eda47a1e3
alaa <aamal79545@gmail.com>
2024-02-02 22:26:26 +0200

    first commit

---
+++ Left rotation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Left_rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr= new int[] {0,1,2,3,4};
+            Left_rotation obj = new Left_rotation();
+            obj.Left(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+ " ");
+            }
+            Console.ReadKey();
+        }
+    }
+    public class Left_rotation
+    {
+        public void Left(int[] arr) 
+        {
+            int X = arr[0];
+            for (int i = 0 ; i < arr.Length-1 ; i++) {
+                arr[i] = arr[i+1];
+            }
+            arr[arr.Length-1] = X;
+        }
+    }
+}
